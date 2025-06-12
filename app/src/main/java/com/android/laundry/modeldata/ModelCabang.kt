9917a5bda74d72f87437089f4c0bf22c5460b018
Mikee-12 <githubmikee12@gmail.com>
2025-06-12 23:43:40 +0700

    penyesuaian bahasa di toast

---
+++ app/src/main/java/com/android/laundry/modeldata/ModelCabang.kt
@@ -0,0 +1,9 @@
+package com.android.laundry.modeldata
+
+data class ModelCabang(
+    var id: String? = null,
+    var namaCabang: String? = null,
+    var manager: String? = null,
+    var alamat: String? = null,
+    var noHp: String? = null
+)
